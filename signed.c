d2fbac311fecb8bf0d0a505c17ff11531e57546c
JBavitha <142578450+JBavitha@users.noreply.github.com>
2023-08-22 10:49:04 +0530

    Add files via upload

---
+++ signed.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	long long int max = (long long int) (pow(2,63) -1);
+	long long int min = (long long int) (pow(2,63) *(-1));
+	printf("Minimum value is %lld\n",min);
+	printf("Max value is %lld\n",max);
+	return 0;
+}
